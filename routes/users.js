c8b29ee286e921d602a8da8fd884924c64b5ba47
Daryl Jewkes <github@daryljewkes.com>
2014-05-25 21:01:47 +0100

    starting to add user list

---
+++ routes/users.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+
+	db.collection('users').find({}).toArray(function (err, items) {
+			if (err) { console.log(err); return res.send(err);}
+			if (items.length == 0) { res.send("No users found"); }
+      		res.send(items);
+		})
+
+return;
+    db.collection('users').find({},{}, function(err, docs){
+    	if (err) res.send(err)
+console.log(docs)
+    	//console.log(docs)
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
+
+/* GET New User page. */
+router.get('/newuser', function(req, res) {
+    res.render('newuser', { title: 'Add New User' });
+});
+
+/* POST to Add User Service */
+router.post('/adduser', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    var userEmail = req.body.useremail;
+
+    // Set our collection
+    var collection = db.get('users');
+
+    // Submit to the DB
+    collection.insert({
+        "username" : userName,
+        "email" : userEmail
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // If it worked, set the header so the address bar doesn't still say /adduser
+            res.location("userlist");
+            // And forward to success page
+            res.redirect("userlist");
+        }
+    });
+});
+
+module.exports = router;
