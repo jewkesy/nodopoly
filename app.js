6c25a92268996c04b6043ca3767684649a7cc687
Daryl Jewkes <github@daryljewkes.com>
2014-06-23 09:48:56 +0100

    started to grab latest version of packages

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.set('view engine', 'jade');
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+//app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
